bf9ca9f492f10d0f3bc05f487ffdccaee3414263
Chitoku <odango@chitoku.jp>
2018-05-09 00:38:59 +0900

    Remove redundant compinit loading

--- config/zsh/plugins/completion.zsh
+++ config/zsh/plugins/completion.zsh
@@ -1,6 +1,3 @@
-autoload -Uz compinit
-
-compinit -u
 zstyle ':completion:*:default' menu select=1
 zstyle ':completion:*' list-colors 'di=34' 'ln=35' 'so=32' 'ex=31' 'bd=46;34' 'cd=43;34'
 zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
