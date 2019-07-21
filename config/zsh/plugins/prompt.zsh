04f914f3540ac788baa854a030782eb4402eb07e
Chitoku <odango@chitoku.jp>
2019-07-21 19:15:06 +0900

    Support Zsh 5.0.2

--- config/zsh/plugins/prompt.zsh
+++ config/zsh/plugins/prompt.zsh
@@ -1,26 +1,75 @@
+autoload -U is-at-least
+
+typeset -gA _zsh_prompt_format
+
+if is-at-least 5.7; then
+    _zsh_prompt_format[normal-hostname]='%K{#8fa1b3}%F{#2b303b} %m %k%f'
+    _zsh_prompt_format[normal-directory]='%K{#4f5b66}%F{#a7adba} %1~ %k%f'
+    _zsh_prompt_format[normal-vcs0]='%%K{#4f5b66}%%F{#a7adba}| %s '
+    _zsh_prompt_format[normal-vcs1]='%%K{#ebcb8b}%%F{#2b303b} %s %%f%%k'
+    _zsh_prompt_format[normal-user]='%F{#a7adba}%(!.%K{#b3666c} # %k.%K{#343d46} $ %k) %f'
+
+    _zsh_prompt_format[insert-hostname]='%K{#a3be8c}%F{#2b303b} %m %k%f'
+    _zsh_prompt_format[insert-directory]=${_zsh_prompt_format[normal-directory]}
+    _zsh_prompt_format[insert-vcs0]=${_zsh_prompt_format[normal-vcs0]}
+    _zsh_prompt_format[insert-vcs1]=${_zsh_prompt_format[normal-vcs1]}
+    _zsh_prompt_format[insert-user]=${_zsh_prompt_format[normal-user]}
+
+    _zsh_prompt_format[finish-hostname]='%K{#65737e}%F{#a7adba} %m %k%f'
+    _zsh_prompt_format[finish-directory]=${_zsh_prompt_format[normal-directory]}
+    _zsh_prompt_format[finish-vcs0]=${_zsh_prompt_format[normal-vcs0]}
+    _zsh_prompt_format[finish-vcs1]='%%K{#65737e}%%F{#a7adba} %s %%f%%k'
+    _zsh_prompt_format[finish-user]=${_zsh_prompt_format[normal-user]}
+else
+    _zsh_prompt_format[normal-hostname]=$'%{\x1b[48;2;143;161;179m\x1b[38;2;43;48;59m%} %m '
+    _zsh_prompt_format[normal-directory]=$'%{\x1b[48;2;79;91;102m\x1b[38;2;167;173;186m%} %1~ '
+    _zsh_prompt_format[normal-vcs0]=$'%%{\x1b[48;2;79;91;102m\x1b[38;2;167;173;186m%%}| %s '
+    _zsh_prompt_format[normal-vcs1]=$'%%{\x1b[48;2;235;203;139m\x1b[38;2;43;48;59m%%} %s '
+    _zsh_prompt_format[normal-user]=$'%{\x1b[38;2;167;173;186m%}%(!.%{\x1b[48;2;179;102;108m%} # .%{\x1b[48;2;52;61;70m%} $ )%{\x1b[0m%} '
+
+    _zsh_prompt_format[insert-hostname]=$'%{\x1b[48;2;163;190;140m\x1b[38;2;43;48;59m%} %m '
+    _zsh_prompt_format[insert-directory]=${_zsh_prompt_format[normal-directory]}
+    _zsh_prompt_format[insert-vcs0]=${_zsh_prompt_format[normal-vcs0]}
+    _zsh_prompt_format[insert-vcs1]=${_zsh_prompt_format[normal-vcs1]}
+    _zsh_prompt_format[insert-user]=${_zsh_prompt_format[normal-user]}
+
+    _zsh_prompt_format[finish-hostname]=$'%{\x1b[48;2;101;115;126m\x1b[38;2;167;173;186m%} %m '
+    _zsh_prompt_format[finish-directory]=${_zsh_prompt_format[normal-directory]}
+    _zsh_prompt_format[finish-vcs0]=${_zsh_prompt_format[normal-vcs0]}
+    _zsh_prompt_format[finish-vcs1]=$'%%{\x1b[48;2;101;115;126m\x1b[38;2;167;173;186m%%} %s %%f%%k'
+    _zsh_prompt_format[finish-user]=${_zsh_prompt_format[normal-user]}
+
+    add-zsh-hook precmd () {
+        LANG=en_US.UTF-8 vcs_info
+        _zsh_prompt 'insert' 2> /dev/null
+    }
+fi
+
 _zsh_prompt_redraw() {
     if [[ "$1" = "0" ]] || [[ "$WIDGET" =~ finish ]]; then
-        _zsh_prompt '#65737e' '#a7adba' '#65737e' '#a7adba'
+        _zsh_prompt 'finish'
         return
     fi
     case "$KEYMAP" in
-        'main')
-            _zsh_prompt '#a3be8c' '#2b303b' '#ebcb8b' '#2b303b'
+        main)
+            _zsh_prompt 'insert'
             ;;
         *)
-            _zsh_prompt '#8fa1b3' '#2b303b' '#ebcb8b' '#2b303b'
+            _zsh_prompt 'normal'
             ;;
     esac
 }
 
 _zsh_prompt() {
-    local hostname="%K{$1}%F{$2} %m %k%f"
-    local directory="%K{#4f5b66}%F{#a7adba} %1~ %k%f"
-    local vcs_info=''
-    local user="%F{#a7adba}%(!.%K{1} # %k.%K{#343d46} $ %k) %f"
+    local vcs_info
+    local hostname=${_zsh_prompt_format[$1-hostname]}
+    local directory=${_zsh_prompt_format[$1-directory]}
+    local vcs0=${_zsh_prompt_format[$1-vcs0]}
+    local vcs1=${_zsh_prompt_format[$1-vcs1]}
+    local user=${_zsh_prompt_format[$1-user]}
 
-    [[ -n "$vcs_info_msg_0_" ]] && vcs_info+="%K{#4f5b66}%F{#a7adba}| $vcs_info_msg_0_ "
-    [[ -n "$vcs_info_msg_1_" ]] && vcs_info+="%K{$3}%F{$4} $vcs_info_msg_1_ %f%k"
+    [[ -n "$vcs_info_msg_0_" ]] && vcs_info+=$(printf "$vcs0" "$vcs_info_msg_0_")
+    [[ -n "$vcs_info_msg_1_" ]] && vcs_info+=$(printf "$vcs1" "$vcs_info_msg_1_")
     [[ -n "$vcs_info" ]] && vcs_info+="%k%f"
 
     PROMPT=$'\n'$hostname$directory$vcs_info$user
