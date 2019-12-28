c4ec7ae67a2b581e9e1e1a379c0d68fd887819de
Chitoku <odango@chitoku.jp>
2019-12-29 03:51:29 +0900

    Add fzf completion functions for git

--- config/zsh/plugins/fzf.zsh
+++ config/zsh/plugins/fzf.zsh
@@ -6,3 +6,38 @@ if [[ -a "$XDG_CACHE_HOME/zplug/repos/nicodebo/base16-fzf/bash/base16-ocean.conf
     source "$XDG_CACHE_HOME/zplug/repos/nicodebo/base16-fzf/bash/base16-ocean.config"
     export FZF_DEFAULT_OPTS="$FZF_DEFAULT_OPTS  --color=bg:-1"
 fi
+
+_fzf_complete_git() {
+    if [[ "$@" = 'git branch'* ]] || [[ "$@" = 'git checkout'* ]]; then
+        _fzf_complete '' "$@" < <(git branch -a --format='%(refname:short)  %(contents:subject)' 2> /dev/null)
+        return
+    fi
+
+    if [[ "$@" = 'git commit'* ]]; then
+        _fzf_complete_git-commit "$@"
+        return
+    fi
+
+    if  [[ "$@" = 'git rebase'* ]] || [[ "$@" = 'git reset'* ]]; then
+        _fzf_complete '' "$@" < <(git log --format='%h  %s' 2> /dev/null)
+        return
+    fi
+
+    zle ${fzf_default_completion:-expand-or-complete}
+}
+
+_fzf_complete_git-commit() {
+    _fzf_complete '' "$@" < <(git log --format='%h  %s' 2> /dev/null)
+}
+
+_fzf_complete_git_post() {
+    awk '{ print $1 }'
+}
+
+_fzf_complete_git-commit_post() {
+    awk '{
+        $1 = ""
+        sub(/^ /, "", $0)
+        print "'\''" $0 "'\''"
+    }'
+}
