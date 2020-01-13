b2957da70cb5f9bef230a4a02e6d775fcd663b32
Chitoku <odango@chitoku.jp>
2020-01-13 13:55:53 +0900

    Disable execute-named-cmd

--- config/zsh/plugins/mappings.zsh
+++ config/zsh/plugins/mappings.zsh
@@ -6,6 +6,7 @@ bindkey "^L" clear-screen
 bindkey "^F" sudo-command-line
 bindkey "^[[3~" delete-char
 bindkey "^[[Z" reverse-menu-complete
+bindkey -M vicmd -r ":"
 bindkey -M viins "^X^N" autosuggest-accept
 
 if [[ -a "$XDG_CONFIG_HOME/fzf/fzf.zsh" ]]; then
