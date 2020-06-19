5124160cca1e171012979711f433d07af938b03f
Chitoku <odango@chitoku.jp>
2020-06-19 19:28:34 +0900

    Configure for BOSH VM

--- config/zsh/plugins/bosh.zsh
+++ config/zsh/plugins/bosh.zsh
@@ -1,3 +1,15 @@
 if [[ -a /var/vcap/instance ]]; then
     HOST=$(cat /var/vcap/instance/name)/$(cat /var/vcap/instance/id)
 fi
+
+if [[ -a /var/vcap/bosh ]]; then
+    monit() {
+        sudo /var/vcap/bosh/bin/monit $@
+    }
+fi
+
+if [[ -a /var/vcap/jobs/bpm/bin ]]; then
+    bpm() {
+        sudo /var/vcap/jobs/bpm/bin/bpm $@
+    }
+fi
