51dc5e8f4e3a3b4a63bec52a3139e5c4b87e1ff7
Chitoku <odango@chitoku.jp>
2020-08-01 02:39:30 +0900

    Configure Vi mode prompt in PowerShell

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -1,14 +1,15 @@
-Set-PSReadLineOption -Colors @{
-    "Command"   = [ConsoleColor]::DarkBlue
-    "Comment"   = [ConsoleColor]::Yellow
-    "Keyword"   = [ConsoleColor]::DarkMagenta
-    "Member"    = [ConsoleColor]::DarkBlue
-    "Number"    = [ConsoleColor]::Red
-    "Operator"  = [ConsoleColor]::DarkCyan
-    "Parameter" = [ConsoleColor]::Red
-    "String"    = [ConsoleColor]::DarkGreen
-    "Type"      = [ConsoleColor]::DarkYellow
-    "Variable"  = [Console]::ForegroundColor
+function OnViModeChange {
+    $global:PSConsoleViModeChanging = $TRUE
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+    $global:PSConsoleViModeChanging = $FALSE
+}
+
+function OnAcceptLine {
+    $global:PSConsoleAccepting = $TRUE
+    [Microsoft.PowerShell.PSConsoleReadLine]::InvokePrompt()
+    $global:PSConsoleAccepting = $FALSE
+
+    [Microsoft.PowerShell.PSConsoleReadLine]::AcceptLine()
 }
 
 function Prompt {
@@ -17,10 +18,44 @@ function Prompt {
         default { Split-Path $executionContext.SessionState.Path.CurrentLocation.Path -Leaf }
     }
 
-    Write-Host
-    Write-Host -NoNewLine -ForegroundColor DarkGreen -BackgroundColor DarkGreen "["
-    Write-Host -NoNewLine -ForegroundColor Black -BackgroundColor DarkGreen $env:COMPUTERNAME" "
-    Write-Host -NoNewLine -ForegroundColor Blue -BackgroundColor Yellow " $path "
+    if (!$global:PSConsoleAccepting -And !$global:PSConsoleViModeChanging) {
+        Write-Host
+    }
 
-    "`e[48;2;52;61;70m $`e[38;2;52;61;70m]`e[0m "
+    if ($global:PSConsoleAccepting) {
+        $cursor = "`e[1 q"
+        $mode = "`e[48;2;101;115;126m`e[38;2;101;115;126m[`e[38;2;167;173;186m"
+    } elseif ([Microsoft.PowerShell.PSConsoleReadLine]::InViCommandMode()) {
+        $cursor = "`e[1 q"
+        $mode = "`e[48;2;143;161;179m`e[38;2;143;161;179m[`e[38;2;43;48;59m"
+    } else {
+        $cursor = "`e[5 q"
+        $mode = "`e[48;2;163;190;140m`e[38;2;163;190;140m[`e[38;2;43;48;59m"
+    }
+
+    $computername = "$env:COMPUTERNAME "
+    $directory = "`e[48;2;79;91;102m`e[38;2;167;173;186m $path "
+    $prompt = "`e[48;2;79;91;102m`e[38;2;167;173;186m`e[48;2;52;61;70m $`e[38;2;52;61;70m]`e[0m "
+
+    "$cursor$mode$computername$directory$prompt"
 }
+
+Set-PSReadLineOption -EditMode Vi
+Set-PSReadLineOption -ViModeIndicator Script -ViModeChangeHandler $Function:OnViModeChange
+Set-PSReadLineOption -Colors @{
+    Command   = [ConsoleColor]::DarkBlue
+    Comment   = [ConsoleColor]::Yellow
+    Keyword   = [ConsoleColor]::DarkMagenta
+    Member    = [ConsoleColor]::DarkBlue
+    Number    = [ConsoleColor]::Red
+    Operator  = [ConsoleColor]::DarkCyan
+    Parameter = [ConsoleColor]::Red
+    String    = [ConsoleColor]::DarkGreen
+    Type      = [ConsoleColor]::DarkYellow
+    Variable  = [Console]::ForegroundColor
+}
+Set-PSReadLineKeyHandler -ViMode Insert -Chord Enter -ScriptBlock { OnAcceptLine }
+Set-PSReadLineKeyHandler -ViMode Command -Chord Enter -ScriptBlock { OnAcceptLine }
+Set-PSReadLineKeyHandler -ViMode Insert -Chord Ctrl+h -Function BackwardDeleteChar
+Set-PSReadLineKeyHandler -ViMode Command -Chord Ctrl+Oem4 -ScriptBlock {}
+Set-PSReadLineKeyHandler -ViMode Insert -Chord Ctrl+Oem4 -Function ViCommandMode
