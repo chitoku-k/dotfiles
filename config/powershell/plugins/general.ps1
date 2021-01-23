6e33474ecfe207bccb13fb3afdc0ef162a2c7bfd
Chitoku <odango@chitoku.jp>
2021-01-24 05:12:53 +0900

    Automatically Set-Location in PSReadLine

--- config/powershell/plugins/general.ps1
+++ config/powershell/plugins/general.ps1
@@ -1,2 +1,23 @@
 Set-PSReadLineOption -BellStyle None
 Set-PSReadLineOption -HistorySaveStyle SaveNothing
+
+$ExecutionContext.InvokeCommand.CommandNotFoundAction = {
+    Param(
+        [String]
+        $name,
+
+        [System.Management.Automation.CommandLookupEventArgs]
+        $commandLookupArgs
+    )
+
+    if ($commandLookupArgs.CommandOrigin -ne "Runspace") {
+        return
+    }
+
+    if (Test-Path -PathType Container -Path $name) {
+        $commandLookupArgs.StopSearch = $true
+        $commandLookupArgs.CommandScriptBlock = {
+            Set-Location $name
+        }.GetNewClosure()
+    }
+}
