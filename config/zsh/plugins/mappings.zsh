ad8609854b05a8116641fc14f5b6825b089cd1dd
Chitoku <odango@chitoku.jp>
2021-10-20 00:48:12 +0900

    Uninstall hcgraf/zsh-sudo

--- config/zsh/plugins/mappings.zsh
+++ config/zsh/plugins/mappings.zsh
@@ -11,7 +11,6 @@ bindkey -r '^D'
 bindkey '^P' history-beginning-search-backward
 bindkey '^N' history-beginning-search-forward
 bindkey '^L' clear-screen
-bindkey '^F' sudo-command-line
 bindkey '^[[3~' delete-char
 bindkey '^[[Z' fzf-tab-complete
 bindkey -M vicmd -r ':'
