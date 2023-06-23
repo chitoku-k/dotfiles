04b4a880dbfe095b7a3b5ff41f528bc1adb4bc19
Chitoku <odango@chitoku.jp>
2023-06-24 03:20:29 +0900

    Detect terminal only using `has('ttyin')`

--- config/nvim/lua/userautoload/colors.lua
+++ config/nvim/lua/userautoload/colors.lua
@@ -8,7 +8,7 @@ if not pcall(vim.cmd.colorscheme, 'base16-ocean') then
   vim.cmd.colorscheme('desert')
 end
 
-if vim.fn.has('ttyin') == 1 or not vim.g.nyaovim_version then
+if vim.fn.has('ttyin') == 1 then
   vim.api.nvim_set_hl(0, 'Normal', {})
 end
 
