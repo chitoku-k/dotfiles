ed47d166fa284fc5f1ff344dd85c832065156221
Chitoku <odango@chitoku.jp>
2023-06-26 19:16:57 +0900

    Configure highlight for vim-better-whitespace from `vim.api.nvim_set_hl()`

--- config/nvim/plugins/hooks/vim-better-whitespace.lua
+++ config/nvim/plugins/hooks/vim-better-whitespace.lua
@@ -1,7 +1,3 @@
--- lua_add {{{
-vim.g.better_whitespace_guicolor = vim.g.base16_gui08
--- }}}
-
 -- lua_post_source {{{
 local filetypes_blacklist = vim.g.better_whitespace_filetypes_blacklist
 table.insert(filetypes_blacklist, 'liquid')
