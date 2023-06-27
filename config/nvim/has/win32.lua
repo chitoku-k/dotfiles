c40258f25f0b6017508fae47c300ceeb3f76bfb2
Chitoku <odango@chitoku.jp>
2023-06-27 17:02:21 +0900

    Remove `vim.opt.clipboard`

--- config/nvim/has/win32.lua
+++ config/nvim/has/win32.lua
@@ -2,10 +2,6 @@ vim.opt.linespace = 4
 vim.opt.guifont = 'Consolas:h10'
 vim.opt.guifontwide = 'Meiryo:h10,MS_Gothic:h10'
 
-vim.schedule(function()
-  vim.opt.clipboard = { 'unnamedplus', 'unnamed' }
-end)
-
 if vim.fn.executable('pwsh') == 1 then
   vim.opt.shell = 'pwsh -NoLogo'
   vim.opt.shellcmdflag = '-NoProfile -NoLogo -NonInteractive -Command'
