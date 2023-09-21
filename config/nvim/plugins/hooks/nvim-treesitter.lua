da65ffce973650c88b5684282997e41c14c2ec4b
Chitoku <odango@chitoku.jp>
2023-09-21 23:15:11 +0900

    Install nvim-treesitter/nvim-treesitter

---
+++ config/nvim/plugins/hooks/nvim-treesitter.lua
@@ -0,0 +1,28 @@
+-- lua_post_source {{{
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'bash',
+    'c',
+    'cpp',
+    'css',
+    'dockerfile',
+    'go',
+    'html',
+    'javascript',
+    'json',
+    'lua',
+    'make',
+    'markdown',
+    'markdown_inline',
+    'ruby',
+    'rust',
+    'sql',
+    'toml',
+    'tsx',
+    'typescript',
+    'vim',
+    'vimdoc',
+    'yaml',
+  },
+})
+-- }}}
