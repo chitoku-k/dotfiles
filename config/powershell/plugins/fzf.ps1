5701bf782590f1e5ea706be7bef2c45fafb96233
Chitoku <odango@chitoku.jp>
2024-06-15 19:26:37 +0900

    Add $Env:FZF_DEFAULT_OPTS for PowerShell

---
+++ config/powershell/plugins/fzf.ps1
@@ -0,0 +1,5 @@
+$Env:FZF_DEFAULT_OPTS = @"
+--color=bg+:#343d46,bg:-1,spinner:#96b5b4,hl:#8fa1b3
+--color=fg:#a7adba,header:#8fa1b3,info:#ebcb8b,pointer:#96b5b4
+--color=marker:#96b5b4,fg+:#dfe1e8,prompt:#ebcb8b,hl+:#8fa1b3
+"@
