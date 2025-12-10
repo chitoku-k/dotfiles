1ef078e4d7028d0e0c9f41c5badeba4f3cf39f16
Chitoku <odango@chitoku.jp>
2025-12-10 18:16:56 +0900

    Enable vim-better-whitespace in Markdown files

--- config/nvim/plugins/hooks/vim-better-whitespace.lua
+++ config/nvim/plugins/hooks/vim-better-whitespace.lua
@@ -1,5 +1,9 @@
 -- lua_post_source {{{
-local filetypes_blacklist = vim.g.better_whitespace_filetypes_blacklist
-table.insert(filetypes_blacklist, 'liquid')
+local filetypes_blacklist = {'liquid'}
+for _, filetype in ipairs(vim.g.better_whitespace_filetypes_blacklist) do
+  if filetype ~= 'markdown' then
+    table.insert(filetypes_blacklist, filetype)
+  end
+end
 vim.g.better_whitespace_filetypes_blacklist = filetypes_blacklist
 -- }}}
